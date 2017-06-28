16a6476112dfa63d86d64bb05486daecb260e494
deolson <dolson77@cox.net>
2017-06-28 00:39:52 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(8080, () => console.log('Server running on port 8080'));
